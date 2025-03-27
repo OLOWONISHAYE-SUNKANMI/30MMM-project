81b0e411244137565de007b902a4f5f0667a739a
vic1157 <ariyo90@gmail.com>
2025-03-27 08:03:43 -0400

    feat(Pledge Donation): Added Pledge Widget
    Added Donation Widget to Showcase to Pledge Team
    - This commit is for demo purposes
    - Pledge team wanted to see embed format (button vs. form)

--- components/Dashboard/MainBody.jsx
+++ components/Dashboard/MainBody.jsx
@@ -11,7 +11,7 @@ export default function MainBody() {
 
   return (
     <div className="relative mx-auto mb-8 mt-24 flex min-h-screen w-full max-w-[1200px] flex-col items-start gap-y-5 space-y-4 pt-12 max-lg:mx-2">
-      <div className="flex w-full flex-wrap items-center justify-start gap-2 md:gap-y-5">
+      <div className="flex flex-wrap items-center justify-start w-full gap-2 md:gap-y-5">
         <h1 className="text-3xl font-bold leading-relaxed md:text-4xl">
           Hello, {userInfo.fullName}
         </h1>
@@ -25,9 +25,11 @@ export default function MainBody() {
           </span>
         </h2>
       </div>
+      {/* Pledge Donation */}
+      <div class="plg-donate" data-widget-id="S1NzAwq1gKUewVE3"></div>
       <DonateHero />
       {/* The sizes by lines are 22 14 12 and the buttons are 16 */}
-      <div className="mr-auto w-full">
+      <div className="w-full mr-auto">
         <h4 className="text-3xl font-semibold leading-7 tracking-wider">
           CLEAN {cohortText}
         </h4>
