a504faedf109e2c17b4a36fc99e5299968e0dd00
vic1157 <ariyo90@gmail.com>
2025-04-16 09:46:15 -0400

    feat(donate): Integrated Pledge Widget to Dashboard
    - Pledge Widget appears when original DonateHero Widget is clicked
    - Working with Zapier to pull donor details to update email groups
    
    Closes #868dfphuv

--- components/Dashboard/MainBody.jsx
+++ components/Dashboard/MainBody.jsx
@@ -3,15 +3,22 @@ import { FaChevronDown } from "react-icons/fa";
 import DonateHero from "@/components/Dashboard/DonateHero";
 import intToRoman from "@/lib/intToRoman";
 import CardSection from "./CardSection";
+import { useState } from "react";
 
 export default function MainBody() {
   const { userInfo, userProgress } = useDashboardContext();
+  const [showPledgeWidget, setShowPledgeWidget] = useState(false);
 
   const cohortText = userInfo.cohortRoman;
 
+  const handleDonateClick = () => {
+    setShowPledgeWidget(true);
+  };
+  
   return (
+    
     <div className="relative mx-auto mb-8 mt-24 flex min-h-screen w-full max-w-[1200px] flex-col items-start gap-y-5 space-y-4 pt-12 max-lg:mx-2">
-      <div className="flex flex-wrap items-center justify-start w-full gap-2 md:gap-y-5">
+      <div className="flex flex-wrap items-center w-full gap-2 md:gap-y-5">
         <h1 className="text-3xl font-bold leading-relaxed md:text-4xl">
           Hello, {userInfo.fullName}
         </h1>
@@ -25,9 +32,16 @@ export default function MainBody() {
           </span>
         </h2>
       </div>
-      {/* Pledge Donation */}
-      <div class="plg-donate" data-widget-id="S1NzAwq1gKUewVE3"></div>
-      <DonateHero />
+      
+      {/* Conditional rendering with improved handling of DonateHero */}
+      {showPledgeWidget ? (
+        <div className="flex justify-center w-full">
+          <div className="plg-donate w-full max-w-[800px]" data-widget-id="S1NzAwq1gKUewVE3"></div>
+        </div>
+      ) : (
+        <DonateHero onClick={handleDonateClick} />
+      )}
+      
       {/* The sizes by lines are 22 14 12 and the buttons are 16 */}
       <div className="w-full mr-auto">
         <h4 className="text-3xl font-semibold leading-7 tracking-wider">
