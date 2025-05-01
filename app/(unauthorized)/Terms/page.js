997207430b2db7bf9d20d53210db6a16a8b41250
Aaron Dantley <aarondantle@gmail.com>
2025-05-01 10:39:30 -0400

    feat: updating Terms page to `Terms of Use`

--- app/(unauthorized)/Terms/page.js
+++ app/(unauthorized)/Terms/page.js
@@ -8,23 +8,22 @@ export default function Terms() {
     <>
       <Head>
         <title>
-          Terms of Service for the CLEAN Program&apos;s Learning Management
-          System
+          Terms of Use for the CLEAN Program&apos;s Learning Management System
         </title>
         <meta
           name="description"
-          content="Terms of Service for CLEAN LMS provided by Thirty Mighty Men Ministries"
+          content="Terms of Use for CLEAN LMS provided by Thirty Mighty Men Ministries"
         />
       </Head>
-      <div className="container max-w-5xl p-6 mx-auto">
+      <div className="container mx-auto max-w-5xl p-6">
         <Image
-          className="z-10 block mx-auto -mt-16 md:mt-16"
+          className="z-10 mx-auto -mt-16 block md:mt-16"
           src="/logo.png"
           alt="Logo"
           width={120}
           height={120}
         />
-        <div className="flex flex-col items-center justify-between min-h-screen p-24">
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
           <TermsSheet />
         </div>
       </div>
