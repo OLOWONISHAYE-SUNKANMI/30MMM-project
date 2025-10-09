88f13c69f892a1fe28ee19ac14c563cc6389cbb1
Aaron Dantley <aarondantley@gmail.com>
2025-10-08 22:28:30 -0400

    feat: confirming user is redirected to login page
    - removed toast message from landing page
    - added toast messaging to login page to display a toast when a user accesses an unauthorized page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { toast } from "sonner";
 import Divider from "@/components/signup-v2/divider";
 import Form from "@/components/signup-v2/form";
 import FormHeader from "@/components/signup-v2/form-header";
 import GoogleBtn from "@/components/signup-v2/google-btn";
 
 export default function Login() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    const message = searchParams.get("message");
+    if (message) {
+      toast.error(message);
+      // Clean up the URL by replacing the current URL without the message parameter
+      const url = new URL(window.location.href);
+      url.searchParams.delete("message");
+      router.replace(url.pathname + url.search);
+    }
+  }, [searchParams, router]);
   return (
     <div className="inset-auto flex min-h-screen w-screen min-w-[400px] flex-col md:flex-row">
       {/* Image Container - Matched to reference styling */}
