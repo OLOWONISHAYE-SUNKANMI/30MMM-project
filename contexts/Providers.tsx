54f6b6a4e8fb333c414dbc9a55358c8884b047c3
Aaron Dantley <aarondantley@gmail.com>
2026-02-09 19:30:40 -0500

    fix: removing double import of Session provider

--- contexts/Providers.tsx
+++ contexts/Providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { SessionProvider } from "next-auth/react";
 import { AuthProvider } from "@/contexts/AuthContext";
 import DashboardProvider from "@/contexts/dashboard/dashboard-provider";
 import { SessionProvider } from "next-auth/react";
